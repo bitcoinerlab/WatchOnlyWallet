46fe0774701e75381ea0fdd2f64155e3a0e367c5
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-10-25 09:49:29 +0200

    feat: improve ui and handle very basic errors

--- app/index.tsx
+++ app/index.tsx
@@ -12,7 +12,7 @@ import {
 } from "@bitcoinerlab/discovery";
 import { DescriptorsFactory } from "@bitcoinerlab/descriptors";
 import * as secp256k1 from "@bitcoinerlab/secp256k1";
-const { Output } = DescriptorsFactory(secp256k1);
+const { Output, expand } = DescriptorsFactory(secp256k1);
 
 import { electrumParams } from "./utils";
 
@@ -29,16 +29,20 @@ export default function Home() {
 
   const connect = useCallback(async () => {
     if (electrumURI) {
-      const params = electrumParams(electrumURI);
-      if (discovery) {
-        await discovery.getExplorer().close();
+      try {
+        if (discovery) {
+          await discovery.getExplorer().close();
+        }
+        setDiscovery(null);
+        const params = electrumParams(electrumURI);
+        const explorer = new ElectrumExplorer({ network, ...params });
+        await explorer.connect();
+        const { Discovery } = DiscoveryFactory(explorer, network);
+        const newDiscovery = new Discovery();
+        setDiscovery(newDiscovery);
+      } catch (err) {
+        setDiscovery(null);
       }
-      setDiscovery(null);
-      const explorer = new ElectrumExplorer({ network, ...params });
-      await explorer.connect();
-      const { Discovery } = DiscoveryFactory(explorer, network);
-      const newDiscovery = new Discovery();
-      setDiscovery(newDiscovery);
     }
   }, [electrumURI, discovery, network]);
   const disconnect = useCallback(async () => {
@@ -55,16 +59,23 @@ export default function Home() {
   }, []);
 
   const fetch = useCallback(async () => {
-    console.log("TRACE: fetching", { discovery: !!discovery });
     if (discovery) {
-      console.log("TRACE: fetching start...");
       setFetching(true);
       await discovery.fetch({ descriptor });
       setFetching(false);
     }
   }, [discovery]);
 
-  const isFetched = discovery?.whenFetched({ descriptor }) || false;
+  let isValidDescriptor = false;
+  try {
+    expand({ descriptor, network });
+    isValidDescriptor = true;
+  } catch (err) {
+    console.log(err);
+  }
+
+  const isFetched =
+    (isValidDescriptor && discovery?.whenFetched({ descriptor })) || false;
   const history = (
     isFetched ? discovery?.getHistory({ descriptor }, true) : []
   ) as TxAttribution[];
@@ -90,11 +101,17 @@ export default function Home() {
           <Text className="mt-2 text-red-500">Not connected</Text>
         )}
         {discovery ? (
-          <Text className="text-red-500 mt-4" onPress={disconnect}>
+          <Text
+            className="py-2 px-4 rounded-lg overflow-hidden text-white bg-red-500 mt-4"
+            onPress={disconnect}
+          >
             Disconnect
           </Text>
         ) : (
-          <Text className="text-blue-500 mt-4" onPress={connect}>
+          <Text
+            className="py-2 px-4 rounded-lg overflow-hidden text-white bg-blue-500 mt-4"
+            onPress={connect}
+          >
             Connect
           </Text>
         )}
@@ -105,12 +122,17 @@ export default function Home() {
           className="border border-gray-300 p-2 rounded-lg w-full mt-1 bg-white"
           placeholder="e.g., wpkh([d34db33f/84h/0h/0h]xpub6.../0/*)"
         />
+        {descriptor !== "" && !isValidDescriptor ? (
+          <Text className="text-red-500 mt-2">
+            This descriptor does not seem to be valid...
+          </Text>
+        ) : null}
         {fetching ? (
           <Text className={`"text-gray-400"} mt-4`}>Fetching...</Text>
         ) : (
           <Text
-            className={`${discovery ? "text-blue-500" : "text-gray-400"} mt-4`}
-            {...(discovery ? { onPress: fetch } : {})}
+            className={`mt-4 py-2 px-4 rounded-lg overflow-hidden ${discovery && isValidDescriptor ? "text-white bg-blue-500" : "text-gray-400 bg-gray-200 opacity-50 cursor-not-allowed"}`}
+            {...(discovery && isValidDescriptor ? { onPress: fetch } : {})}
           >
             Fetch
           </Text>
